b4c6cae13f5cbe93c197b7ac907f86d3293d2849
Eduard <102860662+Hajime-in-heart@users.noreply.github.com>
2023-01-22 00:58:45 +0200

    Add files via upload

---
+++ Autorization/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Autorization
+    {
+    public partial class Form1 : Form
+        {
+        public Form1()
+            {
+            InitializeComponent();
+            }
+
+
+        public void loadDataBase(string userName, string password)
+            {
+
+            // Подключение к БД
+            string pathToDataBase = Application.StartupPath + "/db.accdb";
+            OleDbConnection connectToDataBase = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + pathToDataBase);
+
+
+            // SQL запрос
+            string query = "SELECT [Username], [Password] FROM [Login data] WHERE ([Username]='" + userName + "');";
+
+
+            // Запуск подключения
+            connectToDataBase.Open();
+            OleDbCommand command = new OleDbCommand(query, connectToDataBase); // Отправка команды в БД
+            OleDbDataReader reader = command.ExecuteReader(); // Команда на чтение
+
+
+            // Переменные для вывода из БД верных данных для входа в программу
+            string rightUserName = null, rightPassword = null; 
+
+
+            // Получение данных из БД
+            while (reader.Read())
+                {
+                rightUserName = reader[0].ToString();
+                rightPassword = reader[1].ToString();
+
+                // Проверка валидности введенных данных
+                if (userName == rightUserName && password == rightPassword)
+                    {
+                    Form2 newForm = new Form2();
+                    newForm.Show();
+                    this.Hide();
+                    break;
+                    }
+                }
+
+
+            // Закрытие подключения
+            reader.Close();
+            connectToDataBase.Close();
+
+
+            // Вывод сообщения об ошибке в случае ввода неверных данных
+            if (rightUserName == null || password != rightPassword)
+                {
+                MessageBox.Show("Wrong username / password");
+                }
+            }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+            {
+            }
+
+        // Очистка поля с никнеймом
+        private void textBox1_Click(object sender, EventArgs e)
+            {
+            if (textBox1.Text == "Username")
+                {
+                textBox1.Clear();
+                }
+            }
+
+
+        // Очистка поля с паролем
+        private void textBox2_Click(object sender, EventArgs e)
+            {
+            if (textBox2.Text == "Password")
+                {
+                textBox2.Clear();
+                }
+            }
+
+
+        // Присвоение введенных данных переменным и вызов процедуры авторизации
+        private void button1_Click(object sender, EventArgs e)
+            {
+            string userName = textBox1.Text, password = textBox2.Text;
+            loadDataBase(userName, password);
+            }
+        }
+    }
---
+++ Autorization/Form2.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Autorization
+    {
+    partial class Form2
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 333);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Welcome to the Dar1K\'s program";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Roboto Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(45, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(323, 503);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+            }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        }
+    }
